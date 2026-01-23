20c997f2e2c0c5a1edf5a187d59d4663777a5af2
jlbuild <juliabuildbot@gmail.com>
2026-01-23 16:13:32 +0000

    casacorecxx_jll build 0.4.0+0

--- src/casacorecxx_jll.jl
+++ src/casacorecxx_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("casacorecxx")
-JLLWrappers.@generate_main_file("casacorecxx", UUID("689e2960-4160-5310-ae7f-2d2095288874"))
+JLLWrappers.@generate_main_file("casacorecxx", Base.UUID("689e2960-4160-5310-ae7f-2d2095288874"))
 end  # module casacorecxx_jll
